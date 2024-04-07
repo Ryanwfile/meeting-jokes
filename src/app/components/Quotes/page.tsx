12d289d6afeb3fdf447295a25eec3129beed9184
Ryan Williams <codereese@gamil.com>
2024-04-07 12:38:23 -0400

    updating slider

--- src/app/components/Quotes/page.tsx
+++ src/app/components/Quotes/page.tsx
@@ -8,10 +8,33 @@
 //   </div>
 // }
 import styles from "./styles.module.scss";
-const DUMMY_ARR = ["I'm the first index", "Number 2", "Three here", "four is bored four years ago"]
+import JokeComponent from "../Joke/page";
+const DUMMY_JOKE = {
+  jokeId: '1',
+  title: 'title test TITLE I says title',
+  content: 'content test',
+  author: 'test author',
+  isSafeForWork: false
+}
+
+const DUMMY_JOKE2 = {
+  jokeId: '2',
+  title: 'title2 test TITLE2 I says title',
+  content: 'content test2',
+  author: 'test author2',
+  isSafeForWork: false
+}
+// const DUMMY_ARR = ["I'm the first index", "Number 2", "Three here", "four is bored four years ago"]
+const DUMMY_ARR = [DUMMY_JOKE, DUMMY_JOKE2]
 
 export default function Quotes() {
   return <div className={styles.elementToFadeInAndOut}>
-    {DUMMY_ARR.map((text) => <>{text}</>)}
+    {DUMMY_ARR.map((joke) => <JokeComponent 
+    jokeId={joke.jokeId} 
+    title={joke.title} 
+    content={joke.content} 
+    author={joke.author} 
+    isSafeForWork={joke.isSafeForWork} 
+    key={joke.jokeId} />)}
   </div>
 }
